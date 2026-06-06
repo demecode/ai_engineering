af0b7a05ce73231a021c2f6bdf3da01f8ad0ed57
demecode <dpk.williams@gmail.com>
2026-06-06 20:17:51 +0100

    dev setup

---
+++ target/debug/deps/dev_env-380fcb9c6579a9b1.d
@@ -0,0 +1,5 @@
+/Users/demexavier/code/github/demecode/ai_engineering/target/debug/deps/dev_env-380fcb9c6579a9b1.d: phases/00_setup_tooling/dev_env/code/main.rs
+
+/Users/demexavier/code/github/demecode/ai_engineering/target/debug/deps/dev_env-380fcb9c6579a9b1: phases/00_setup_tooling/dev_env/code/main.rs
+
+phases/00_setup_tooling/dev_env/code/main.rs:
---
+++ target/debug/dev_env.d
@@ -0,0 +1,1 @@
+/Users/demexavier/code/github/demecode/ai_engineering/target/debug/dev_env: /Users/demexavier/code/github/demecode/ai_engineering/phases/00_setup_tooling/dev_env/code/main.rs
